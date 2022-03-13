f9a0e6f2a52085ab19b67e5322961f5fc9b3706d
Lionel Tran <lioneldg92@gmail.com>
2022-03-13 17:25:07 +0100

    Init

---
+++ Overworld.js
@@ -0,0 +1,49 @@
+class Overworld {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+
+      // Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      
+      // Establish the camera person
+      // Now it's on hero but can change!
+      const cameraPerson = this.map.gameObjects.hero;
+
+      // Update all objects
+      // Object.values(this.map.gameObjects).forEach(object => {
+      // });
+      
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+      
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction
+        });
+        object.sprite.draw(this.ctx, cameraPerson);
+      });
+
+      this.map.drawUpperImage(this.ctx, cameraPerson);
+
+      requestAnimationFrame(() => {
+        step();
+      });
+    }
+    step();
+  }
+
+  init() {
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
+
+    this.startGameLoop();
+  }
+}
